4c9ad656b50728982083f9570314f4764b51ce00
ParrishJ <jarparrish@gmail.com>
2019-04-22 15:56:30 -0700

    MVP Complete

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return ( <div className="headerTitles">
+        <h4>Lambda School   </h4>
+        <h5>@LambdaSchool - 26 Jan</h5>
+        </div>
+);
+}
+
+export default HeaderTitle;
